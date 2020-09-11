3a2b6072f735bf3e9004786a09e22b00b2533dec
breemark <britomaximiliano83@gmail.com>
2020-09-11 21:51:20 +0700

    localization working, now add router

---
+++ src/components/LocaleSwitcher.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for="locale in locales" :key="locale" @click="switchLocale(locale)">{{locale}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "LocaleSwitcher",
+  methods: {
+    switchLocale(locale) {
+      if (this.$i18n.locale !== locale) {
+        this.$i18n.locale = locale;
+      }
+    },
+  },
+  data() {
+    return {
+      locales: process.env.VUE_APP_I18N_SUPPORTED_LOCALE.split(","),
+    };
+  },
+};
+</script>
+
+<style scoped>
+li {
+  text-decoration: underline;
+  list-style-type: none;
+  color: #459ce7;
+  cursor: pointer;
+}
+</style>
